df2bae2d2642b7451573328e5b58d8e95ee27a47
Harsh-0805 <harshbadala0805@gmail.com>
2023-03-07 23:38:46 +0530

    feat(landing)

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-
+import Landing from './landing'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <div>
-      <span>Welcome to ECELL</span>
+      <Landing/>
     </div>
   )
 }
